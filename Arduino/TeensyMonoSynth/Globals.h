c86d21379eb8cc6802aa13fbc203607dd0f60889
MarshallMultispreadDaniel <62632344+MarshallMultispreadDaniel@users.noreply.github.com>
2024-12-27 11:04:59 +0800

    I think that is right

--- Arduino/TeensyMonoSynth/Globals.h
+++ Arduino/TeensyMonoSynth/Globals.h
@@ -4,3 +4,5 @@ uint8_t filter_lfo_depth_cc_value = 0;
 float glide_time_ms = 0;
 uint8_t held_notes[HELD_NOTES_COUNT];
 bool is_legato_enabled = true;
+float detune_amount = 0.001;
+float semitone_offset = 0.0;
